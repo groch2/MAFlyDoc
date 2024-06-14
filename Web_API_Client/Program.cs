c5b93ce5965e1b800a4b2baad709b6190ea94cad
Roch Deschaseaux <roch.deschaseaux@maf.fr>
2024-06-14 15:43:09 +0200

    mise à jour de l'application de test de client MAFlyDoc pour créer des envois

--- Web_API_Client/Program.cs
+++ Web_API_Client/Program.cs
@@ -1,33 +1,40 @@
-﻿var httpClient = new HttpClient();
+﻿using GECO.Services.External.Api.FlyDoc;
+
+var httpClient = new HttpClient { BaseAddress = new Uri("https://api-maflydoc-intra.int.maf.local/") };
 var maflyDocClient =
-    new MyApplication.MAFlyDocClient("https://api-maflydoc-intra.int.maf.local/", httpClient);
-var envoiId = await maflyDocClient.EnvoiAvecAdresseDuDestinataireEnClairAsync(
-    new MAFlyDocClient.EnvoiAvecAdresseDuDestinataireEnClairCommand
-    {
-        Subject = "",
-        Sender = new MAFlyDocClient.SenderEnvoiCommand
-        {
-            PersonFirstName = "John",
-            PersonLastName = "Smith",
-            CompanyName = "MAF",
-            UserId = "john.smith@maf.fr"
-        },
-        AdresseDuDestinataireLignes = new[] {
+    new MAFlyDocApiClient(httpClient);
+var response = await maflyDocClient.EnvoiAvecAdresseDuDestinataireEnClairAsync(
+    new EnvoiAvecAdresseDuDestinataireEnClairCommand
+    (
+        @subject: "",
+        @sender: new SenderEnvoiCommand
+        (
+            @personFirstName: "John",
+            @personLastName: "Smith",
+            @companyName: "MAF",
+            @userId: "john.smith@maf.fr"
+        ),
+        @adresseDuDestinataireLignes: new[] {
                 "monsieur X",
                 "1 rue Bidule",
                 "75001 Paris",
                 "France"
             },
-        MainDocumentGedId = "20240417104202845140274012",
-        AttachementsGedIdList = Array.Empty<string>(),
-        MailPostage = MAFlyDocClient.MailPostage.ENVOI_SIMPLE,
-        Impression = null
-    });
+        @mainDocumentGedId: "20240417104202845140274012",
+        @attachementsGedIdList: Array.Empty<string>(),
+        @mailPostage: MailPostage.ENVOI_SIMPLE,
+        @impression: null,
+        application: null
+    ));
+var location =
+    response.Headers.First(
+        header => string.Equals(header.Key, "location", StringComparison.OrdinalIgnoreCase))
+    .Value.First();
 //var envoi =
 //    await maflyDocClient
 //        .EnvoisFromEnvoisIdListAsync(
 //            comma_separated_envois_id_list: "68,75,74",
 //            with_etat_envoi_history: false);
 //Console.WriteLine(JsonConvert.SerializeObject(envoi, Formatting.Indented));
-Console.WriteLine(new { envoiId });
+Console.WriteLine(new { location });
 Console.ReadKey(true);
