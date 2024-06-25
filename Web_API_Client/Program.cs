39df3762f0dfb5c8c3c9e419a4de48723a657d80
Roch Deschaseaux <roch.deschaseaux@maf.fr>
2024-06-25 14:40:28 +0200

    projet de test de génération de client de la web api

--- Web_API_Client/Program.cs
+++ Web_API_Client/Program.cs
@@ -1,40 +1,16 @@
 ﻿using MAFlyDoc;
+using System.Text.Json;
 
-var httpClient = new HttpClient { BaseAddress = new Uri("https://api-maflydoc-intra.int.maf.local/") };
+var httpClient = new HttpClient { BaseAddress = new Uri("https://api-maflydoc-intra.hom.maf.local/") };
 var maflyDocClient =
     new MAFlyDocApiClient(httpClient);
-var response = await maflyDocClient.EnvoiAvecAdresseDuDestinataireEnClairAsync(
-    new EnvoiAvecAdresseDuDestinataireEnClairCommand
-    (
-        @subject: "",
-        @sender: new SenderEnvoiCommand
-        (
-            @personFirstName: "John",
-            @personLastName: "Smith",
-            @companyName: "MAF",
-            @userId: "john.smith@maf.fr"
-        ),
-        @adresseDuDestinataireLignes: new[] {
-                "monsieur X",
-                "1 rue Bidule",
-                "75001 Paris",
-                "France"
-            },
-        @mainDocumentGedId: "20240417104202845140274012",
-        @attachementsGedIdList: Array.Empty<string>(),
-        @mailPostage: MailPostage.ENVOI_SIMPLE,
-        @impression: null,
-        application: null
-    ));
-var location =
-    response.Headers.First(
-        header => string.Equals(header.Key, "location", StringComparison.OrdinalIgnoreCase))
-    .Value.First();
-//var envoi =
-//    await maflyDocClient
-//        .EnvoisFromEnvoisIdListAsync(
-//            comma_separated_envois_id_list: "68,75,74",
-//            with_etat_envoi_history: false);
-//Console.WriteLine(JsonConvert.SerializeObject(envoi, Formatting.Indented));
-Console.WriteLine(new { location });
+var httpResponse =
+    await maflyDocClient
+        .EnvoisFromEnvoisIdListAsync(
+            comma_separated_envois_id_list: "2026,2028,2029",
+            with_etat_envoi_history: false);
+var envois = httpResponse.Result;
+Console.WriteLine(
+    JsonSerializer.Serialize(
+        envois, new JsonSerializerOptions { WriteIndented = true }));
 Console.ReadKey(true);
